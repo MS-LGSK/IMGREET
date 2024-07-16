d98e69817575a96a839e266bfc9cb20ed49e2c6d
dae <cys964@gmail.com>
2024-07-16 15:26:22 +0900

    Start Project

---
+++ IMGREET/src/main/java/com/lgsk/imgreet/ImgreetApplication.java
@@ -0,0 +1,13 @@
+package com.lgsk.imgreet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImgreetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImgreetApplication.class, args);
+    }
+
+}
---
+++ IMGREET/src/test/java/com/lgsk/imgreet/ImgreetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgsk.imgreet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImgreetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
