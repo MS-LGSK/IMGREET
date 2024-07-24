590d2d0cf4fce8963d039b1aa87178cf1bc75527
Chani17 <cucute8878@naver.com>
2024-07-24 16:27:19 +0900

    Feat: invitation creation page api

--- src/main/java/com/lgsk/imgreet/component/controller/ComponentController.java
+++ src/main/java/com/lgsk/imgreet/component/controller/ComponentController.java
@@ -1,4 +1,17 @@
 package com.lgsk.imgreet.component.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequiredArgsConstructor
 public class ComponentController {
+
+    @GetMapping("/start")
+    public String getCreateGreetPage() {
+        return "createGreet";
+    }
 }
