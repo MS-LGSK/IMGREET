d9e1d82fbe1e79f3558ce7d583c82adccac07307
Chani17 <cucute8878@naver.com>
2024-07-24 18:01:26 +0900

    Test: get all category id

--- src/test/java/com/lgsk/imgreet/category/repository/CategoryRepositoryTest.java
+++ src/test/java/com/lgsk/imgreet/category/repository/CategoryRepositoryTest.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @SpringBootTest
 class CategoryRepositoryTest {
 
@@ -20,5 +22,23 @@ class CategoryRepositoryTest {
     void save() {
         Category category = Category.category("Text", "가로 텍스트", true);
         categoryRepository.save(category);
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("모든 카테고리 아이디 가져오기")
+    void getAllCategoryId() {
+        // given
+        Category category_text = Category.category("Text", "가로 텍스트", true);
+        Category category_shape = Category.category("Shape", "원", true);
+
+        categoryRepository.save(category_text);
+        categoryRepository.save(category_shape);
+
+        // when
+        List<Long> categoryIdList = categoryRepository.findAllCategoryId();
+
+        // then
+        Assertions.assertThat(categoryIdList.size()).isEqualTo(2);
     }
 }
