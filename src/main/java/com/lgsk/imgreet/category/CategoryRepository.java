4d7d45cfed2e05f9086b8c987837cb24db91fbdc
Chani17 <cucute8878@naver.com>
2024-07-25 16:12:48 +0900

    Remove: one of two CategoryRepository

--- src/main/java/com/lgsk/imgreet/category/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lgsk.imgreet.category;
-
-import com.lgsk.imgreet.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
