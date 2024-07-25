a9ec9d9f8da4bd9a6f6188e96a156bcf349bf36e
Chani17 <cucute8878@naver.com>
2024-07-25 10:53:39 +0900

    Add: category detail file initial setting

---
+++ src/main/java/com/lgsk/imgreet/categoryDetail/repository/CategoryDetailRepository.java
@@ -0,0 +1,7 @@
+package com.lgsk.imgreet.categoryDetail.repository;
+
+import com.lgsk.imgreet.entity.CategoryDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryDetailRepository extends JpaRepository<CategoryDetail, Long> {
+}
