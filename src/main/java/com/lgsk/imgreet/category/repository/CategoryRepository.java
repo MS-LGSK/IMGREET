d793daded0acb45197777c8b5701b7e740f335fb
Chani17 <cucute8878@naver.com>
2024-07-25 15:42:37 +0900

    Remove: findAll in Category Repository

--- src/main/java/com/lgsk/imgreet/category/repository/CategoryRepository.java
+++ src/main/java/com/lgsk/imgreet/category/repository/CategoryRepository.java
@@ -2,12 +2,7 @@ package com.lgsk.imgreet.category.repository;
 
 import com.lgsk.imgreet.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-
-    List<Category> findAll();
 
 }
