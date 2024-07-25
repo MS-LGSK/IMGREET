e60336f260b2670bf79b353949804f63166f63d5
이다영 <yooung513@gmail.com>
2024-07-26 04:12:56 +0900

    Update Status 상태코드 전달 구현

--- src/main/java/com/lgsk/imgreet/base/config/SecurityConfig.java
+++ src/main/java/com/lgsk/imgreet/base/config/SecurityConfig.java
@@ -4,10 +4,12 @@ import com.lgsk.imgreet.login.service.OAuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
 @EnableWebSecurity
@@ -27,21 +29,28 @@ public class SecurityConfig {
 
                 // 경로별 인가 작업
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/css/**", "/js/**").permitAll()
-//                        .requestMatchers("/**").permitAll())
-                        .requestMatchers("/", "/oauth2/**", "/login/**").permitAll()
+                        .requestMatchers("/css/**", "/js/**", "/images/**").permitAll()
+                        // 개발 시 모든 접근 허용 (추후 변경 필요)
+                        .requestMatchers("/**").permitAll()
+//                        .requestMatchers("/", "/oauth2/**", "/login/**").permitAll()
                         .anyRequest().authenticated())
 
                 .oauth2Login(oauth2 -> oauth2
                         .loginPage("/login")
                         .userInfoEndpoint(userInfoEndpointConfig ->
                                 userInfoEndpointConfig.userService(oAuthService))
-                        .defaultSuccessUrl("/", true))
+                        .defaultSuccessUrl("/", true)
+                        .failureHandler(((request, response, exception) -> {
+                            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+                            response.getWriter().write("로그인에 실패했습니다.");})))
+
+                .exceptionHandling(exceptionHandling ->
+                        exceptionHandling
+                                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
 
                 .logout( logout -> logout
                         .logoutUrl("/logout")
-                        .logoutSuccessUrl("/")
-                );
+                        .logoutSuccessUrl("/"));
 
         return http.build();
     }
