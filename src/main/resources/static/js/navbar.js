7516f37e3aee12ac37f656912f54325e25e6d01d
dae <cys964@gmail.com>
2024-07-25 09:18:07 +0900

    navbar 햄버거버튼 슬라이드 기능

--- src/main/resources/static/js/navbar.js
+++ src/main/resources/static/js/navbar.js
@@ -1,4 +1,34 @@
 function toggleMenu() {
-    const dropdownMenu = document.getElementById('dropdownMenu');
-    dropdownMenu.classList.toggle('show');
+    const dropdownMenu = document.getElementById("dropdownMenu");
+    const dropdownContainer = document.getElementById("dropdownContainer");
+
+    if (dropdownMenu.classList.contains("show")) {
+        dropdownMenu.classList.remove("show");
+        dropdownMenu.classList.add("hide");
+        setTimeout(() => {
+            dropdownMenu.classList.remove("hide");
+        }, 300); // 애니메이션 시간과 동일하게 설정
+    } else {
+        dropdownMenu.style.display = 'flex'; // 요소를 표시
+        dropdownMenu.classList.add("show");
+        dropdownContainer.classList.add("show");
+    }
 }
+
+document.addEventListener("click", function(event) {
+    const dropdownContainer = document.getElementById("dropdownContainer");
+    const dropdownMenu = document.getElementById("dropdownMenu");
+    const hamburgerBtn = document.querySelector(".hamburger-btn");
+
+    if (!dropdownMenu.contains(event.target) && !hamburgerBtn.contains(event.target)) {
+        if (dropdownMenu.classList.contains("show")) {
+            dropdownMenu.classList.remove("show");
+            dropdownMenu.classList.add("hide");
+            setTimeout(() => {
+                dropdownMenu.classList.remove("hide");
+                dropdownMenu.style.display = 'none'; // 애니메이션이 완료된 후 요소 숨김
+                dropdownContainer.classList.remove("show");
+            }, 300); // 애니메이션 시간과 동일하게 설정
+        }
+    }
+});
