ead59709bb30adc53b13ca5cfff4fe79a9f0653f
이다영 <yooung513@gmail.com>
2024-07-25 21:42:29 +0900

    Update 로그인 기능 구현 완료

--- src/main/java/com/lgsk/imgreet/base/config/SecurityConfig.java
+++ src/main/java/com/lgsk/imgreet/base/config/SecurityConfig.java
@@ -2,12 +2,10 @@ package com.lgsk.imgreet.base.config;
 
 import com.lgsk.imgreet.login.service.OAuthService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -20,12 +18,6 @@ public class SecurityConfig {
     private final OAuthService oAuthService;
 
     @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return web -> web.ignoring()
-                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
-    }
-
-    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .cors(cors -> cors.configurationSource(corsConfig.corsConfigurationSource()))
@@ -35,17 +27,21 @@ public class SecurityConfig {
 
                 // 경로별 인가 작업
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/**").permitAll())
-//                        .requestMatchers("/", "/oauth2/**", "/login/**").permitAll()
-//                        .anyRequest().authenticated())
+                        .requestMatchers("/css/**", "/js/**").permitAll()
+//                        .requestMatchers("/**").permitAll())
+                        .requestMatchers("/", "/oauth2/**", "/login/**").permitAll()
+                        .anyRequest().authenticated())
 
                 .oauth2Login(oauth2 -> oauth2
                         .loginPage("/login")
                         .userInfoEndpoint(userInfoEndpointConfig ->
-                                userInfoEndpointConfig.userService(oAuthService)))
+                                userInfoEndpointConfig.userService(oAuthService))
+                        .defaultSuccessUrl("/", true))
 
-                .logout( logout ->
-                        logout.logoutUrl("/logout"));
+                .logout( logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/")
+                );
 
         return http.build();
     }
