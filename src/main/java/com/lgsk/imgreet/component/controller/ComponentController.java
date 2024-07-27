5a0e744e5a73a63425e6f37f25dfa9706119b2a1
Chani17 <cucute8878@naver.com>
2024-07-27 14:54:39 +0900

    Feat: save component in greet

--- src/main/java/com/lgsk/imgreet/component/controller/ComponentController.java
+++ src/main/java/com/lgsk/imgreet/component/controller/ComponentController.java
@@ -1,17 +1,27 @@
 package com.lgsk.imgreet.component.controller;
 
+import com.lgsk.imgreet.component.service.ComponentService;
+import com.lgsk.imgreet.component.model.ComponentDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @Controller
 @RequiredArgsConstructor
 public class ComponentController {
 
+    private final ComponentService componentService;
+
     @GetMapping("/start")
     public String getCreateGreetPage() {
         return "createGreet";
+    }
+
+    @PostMapping("/greet/{greet_id}/component")
+    public void saveComponent(@PathVariable("greet_id") Long greetId, @RequestBody List<ComponentDTO> componentDTO) {
+        componentService.saveComponent(greetId, componentDTO);
     }
 }
---
+++ src/main/java/com/lgsk/imgreet/component/model/ComponentDTO.java
@@ -0,0 +1,24 @@
+package com.lgsk.imgreet.component.model;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ComponentDTO {
+
+    private String content;
+
+    private float x;
+
+    private float y;
+
+    private float width;
+
+    private float height;
+
+    private float rotation;
+
+    private Long categoryDetailId;
+}
