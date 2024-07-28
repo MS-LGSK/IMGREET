a4df212d6e15e658f9dc028a4d6b4796cf06fdde
이다영 <yooung513@gmail.com>
2024-07-29 05:11:48 +0900

    Delete 필요없는 파일 제거

--- src/main/java/com/lgsk/imgreet/share/repository/ShareRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lgsk.imgreet.share.repository;
-
-import com.lgsk.imgreet.entity.Greet;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShareRepository extends JpaRepository<Greet, Long> {
-}
