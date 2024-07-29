cf04cd5a9c6df7afd836345a30777e88e359f0da
dae <cys964@gmail.com>
2024-07-29 15:44:01 +0900

    location dto

---
+++ src/main/java/com/lgsk/imgreet/share/LocationDTO.java
@@ -0,0 +1,15 @@
+package com.lgsk.imgreet.share;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class LocationDTO {
+    private final double latitude;
+    private final double longitude;
+
+    public String json() {
+        return String.format("{\"latitude\": %f, \"longitude\": %f}", latitude, longitude);
+    }
+}
