cc5365d7c611d0218d50862985757ec11af4598c
daeyong <cys964@gmail.com>
2024-07-30 10:39:07 +0900

    LocationController 생성자 제거
    빌더로 변경

--- src/main/java/com/lgsk/imgreet/category/controller/LocationController.java
+++ src/main/java/com/lgsk/imgreet/category/controller/LocationController.java
@@ -1,14 +1,19 @@
 package com.lgsk.imgreet.category.controller;
 
 import com.lgsk.imgreet.category.dto.LocationDTO;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LocationController {
 
-    @GetMapping("/location")
-    public String getLocation() {
-        return new LocationDTO(37.582354, 127.001886).json();
-    }
+	@GetMapping("/location")
+	public String getLocation() {
+		LocationDTO location = LocationDTO.builder()
+			.latitude(37.582354)
+			.longitude(127.001886)
+			.build();
+		return location.json();
+	}
 }
