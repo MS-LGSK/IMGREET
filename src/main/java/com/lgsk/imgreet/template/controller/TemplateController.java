dd45517ade1910d5dc9239075e9032a8b2332765
Chani17 <cucute8878@naver.com>
2024-07-31 21:11:34 +0900

    Add: user NullpointerException, componentDTOList

--- src/main/java/com/lgsk/imgreet/template/controller/TemplateController.java
+++ src/main/java/com/lgsk/imgreet/template/controller/TemplateController.java
@@ -1,11 +1,16 @@
 package com.lgsk.imgreet.template.controller;
 
 import com.lgsk.imgreet.base.commonUtil.Rq;
+import com.lgsk.imgreet.component.service.ComponentService;
 import com.lgsk.imgreet.entity.User;
 import com.lgsk.imgreet.template.service.TemplateService;
+import com.lgsk.imgreet.component.model.ComponentDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -15,12 +20,13 @@ public class TemplateController {
     private final Rq rq;
 
     @PostMapping("/template/save")
-    public String saveTemplate() {
+    public String saveTemplate(@RequestBody List<ComponentDTO> componentDTOList) {
         User user = rq.getUser();
 
-        if(user != null) {
-            templateService.saveTemplate(user.getId());
-        }
+        if (user == null) throw new NullPointerException("로그인 한 user만 사용할 수 있습니다.");
+
+        templateService.saveTemplate(user.getId(), componentDTOList);
+
         return "main";
     }
 }
