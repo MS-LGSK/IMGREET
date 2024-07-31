51e09102a6ab8f228f3de69d43e57d7a72b74f17
Chani17 <cucute8878@naver.com>
2024-07-31 16:14:11 +0900

    Feat: save template

--- src/main/java/com/lgsk/imgreet/template/controller/TemplateController.java
+++ src/main/java/com/lgsk/imgreet/template/controller/TemplateController.java
@@ -1,4 +1,26 @@
 package com.lgsk.imgreet.template.controller;
 
+import com.lgsk.imgreet.base.commonUtil.Rq;
+import com.lgsk.imgreet.entity.User;
+import com.lgsk.imgreet.template.service.TemplateService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
 public class TemplateController {
+
+    private final TemplateService templateService;
+    private final Rq rq;
+
+    @PostMapping("/template/save")
+    public String saveTemplate() {
+        User user = rq.getUser();
+
+        if(user != null) {
+            templateService.saveTemplate(user.getId());
+        }
+        return "main";
+    }
 }
--- src/main/java/com/lgsk/imgreet/template/service/TemplateService.java
+++ src/main/java/com/lgsk/imgreet/template/service/TemplateService.java
@@ -1,4 +1,30 @@
 package com.lgsk.imgreet.template.service;
 
+import com.lgsk.imgreet.entity.Template;
+import com.lgsk.imgreet.entity.User;
+import com.lgsk.imgreet.login.repository.UserRepository;
+
+import com.lgsk.imgreet.template.repository.TemplateRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class TemplateService {
+
+    private final TemplateRepository templateRepository;
+    private final UserRepository userRepository;
+
+    public void saveTemplate(Long userId) {
+        User findUser = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalStateException("존재하지 않는 회원입니다."));
+
+        Template template = Template.builder()
+                .creatorId(findUser.getId())
+                .build();
+
+        templateRepository.save(template);
+    }
 }
