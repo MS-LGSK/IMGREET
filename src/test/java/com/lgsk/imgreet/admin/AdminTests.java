055c51bf57448b0093b4a86e605e03ea68100ba2
이다영 <yooung513@gmail.com>
2024-08-02 02:48:36 +0900

    Update 관리자 권한 부여 및 철회

--- src/test/java/com/lgsk/imgreet/admin/AdminTests.java
+++ src/test/java/com/lgsk/imgreet/admin/AdminTests.java
@@ -11,17 +11,19 @@ import com.lgsk.imgreet.base.entity.Role;
 import com.lgsk.imgreet.entity.User;
 import com.lgsk.imgreet.login.repository.UserRepository;
 
+import jakarta.transaction.Transactional;
+
 @SpringBootTest
 public class AdminTests {
 
 	@Autowired private UserRepository userRepository;
 
 	@Test
-	// @Transactional
+	@Transactional
 	@DisplayName("관리자 권한 부여하기")
 	void assignAdmin() {
 
-		String name = "이다영";
+		String name = "이름";
 		User user = userRepository.findByNickname(name);
 
 		User updateUser = user.toBuilder()
@@ -31,5 +33,22 @@ public class AdminTests {
 
 		User checkUser = userRepository.findByNickname(name);
 		assertThat(checkUser.getRole()).isEqualTo(Role.ADMIN);
+	}
+
+	@Test
+	@Transactional
+	@DisplayName("관리자 권한 철회하기")
+	void revokeAdmin() {
+
+		String name = "이름";
+		User user = userRepository.findByNickname(name);
+
+		User updateUser = user.toBuilder()
+			.role(Role.FREE_USER)
+			.build();
+		userRepository.save(updateUser);
+
+		User checkUser = userRepository.findByNickname(name);
+		assertThat(checkUser.getRole()).isEqualTo(Role.FREE_USER);
 	}
 }
