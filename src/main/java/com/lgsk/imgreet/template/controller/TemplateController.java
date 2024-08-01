05cb6d21171c3c959bd8a2222b3d85d35a220365
Chani17 <cucute8878@naver.com>
2024-08-01 11:52:06 +0900

    Update: change saveTemplate return type

--- src/main/java/com/lgsk/imgreet/template/controller/TemplateController.java
+++ src/main/java/com/lgsk/imgreet/template/controller/TemplateController.java
@@ -1,32 +1,35 @@
 package com.lgsk.imgreet.template.controller;
 
 import com.lgsk.imgreet.base.commonUtil.Rq;
-import com.lgsk.imgreet.component.service.ComponentService;
 import com.lgsk.imgreet.component.dto.ComponentDTO;
 import com.lgsk.imgreet.entity.User;
 import com.lgsk.imgreet.template.service.TemplateService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@Log4j2
 public class TemplateController {
 
     private final TemplateService templateService;
     private final Rq rq;
 
     @PostMapping("/template/save")
-    public String saveTemplate(@RequestBody List<ComponentDTO> componentDTOList) {
+    public ResponseEntity saveTemplate(@RequestBody List<ComponentDTO> componentDTOList) {
         User user = rq.getUser();
 
         if (user == null) throw new NullPointerException("로그인 한 user만 사용할 수 있습니다.");
 
         templateService.saveTemplate(user.getId(), componentDTOList);
 
-        return "main";
+        return ResponseEntity.ok("template 저장 성공");
     }
 }
