bd6c90073867ca42dcbd1cd2ec96bd50832eb7d8
Chani17 <cucute8878@naver.com>
2024-08-01 20:26:57 +0900

    Feat: connect main page template functionality

--- src/main/java/com/lgsk/imgreet/template/service/TemplateService.java
+++ src/main/java/com/lgsk/imgreet/template/service/TemplateService.java
@@ -2,6 +2,7 @@ package com.lgsk.imgreet.template.service;
 
 import com.lgsk.imgreet.categoryDetail.repository.CategoryDetailRepository;
 import com.lgsk.imgreet.component.dto.ComponentDTO;
+import com.lgsk.imgreet.component.dto.ComponentResponseDTO;
 import com.lgsk.imgreet.component.repository.ComponentRepository;
 import com.lgsk.imgreet.component.service.ComponentService;
 import com.lgsk.imgreet.entity.CategoryDetail;
@@ -10,6 +11,7 @@ import com.lgsk.imgreet.entity.Template;
 import com.lgsk.imgreet.entity.User;
 import com.lgsk.imgreet.login.repository.UserRepository;
 
+import com.lgsk.imgreet.template.dto.TemplateResponseDTO;
 import com.lgsk.imgreet.template.repository.TemplateRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -43,5 +45,26 @@ public class TemplateService {
 
         // 생성된 template id를 가지고 template component 저장
         componentService.saveTemplateComponent(savedTemplate.getId(), componentDTOList);
+    }
+
+    @Transactional(readOnly = true)
+    public List<TemplateResponseDTO> getAllTemplates() {
+        List<Template> templateList = templateRepository.findAll();
+
+        return templateList.stream()
+                .map(template -> TemplateResponseDTO.builder()
+                        .id(template.getId())
+                        .title(template.getTitle())
+                        .imageUrl(template.getImageUrl())
+                        .build())
+                .collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public List<ComponentResponseDTO> getTemplate(Long templateId) {
+        Template template = templateRepository.findById(templateId)
+                .orElseThrow(() -> new IllegalStateException("존재하지 않는 템플릿입니다."));
+
+        return componentService.getTemplateComponent(template.getId());
     }
 }
