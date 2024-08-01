5fab49f9e6acdbc4d5833ee6300c4cfbe907beff
havegrit <shin_jaean@naver.com>
2024-08-01 17:33:44 +0900

    refactor: 임시 저장 기능과 저장 기능 분리

--- src/main/java/com/lgsk/imgreet/greet/controller/GreetController.java
+++ src/main/java/com/lgsk/imgreet/greet/controller/GreetController.java
@@ -1,28 +1,53 @@
 package com.lgsk.imgreet.greet.controller;
 
-import com.lgsk.imgreet.greet.dto.GreetRequestDTO;
+import com.lgsk.imgreet.entity.Greet;
+import com.lgsk.imgreet.greet.dto.CreateGreetRequestDTO;
+import com.lgsk.imgreet.greet.dto.UpdateGreetRequestDTO;
 import com.lgsk.imgreet.greet.service.GreetService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.naming.LimitExceededException;
+import java.nio.file.attribute.UserPrincipalNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class GreetController {
 
     private final GreetService greetService;
 
-    @PostMapping("/greet")
-    public String saveGrit(@RequestBody GreetRequestDTO greetRequestDTO) {
+    @PostMapping("/greet/draft")
+    @PreAuthorize("hasAnyAuthority()")
+    public ResponseEntity<Object> temporarySaveGrit(@RequestBody CreateGreetRequestDTO createGreetRequestDTO) {
+        Map<String, Object> responseMap = new HashMap<>();
         try {
-            greetService.saveGreet(greetRequestDTO);
-        } catch (LimitExceededException e) {
-            // TODO: global exception handler 처리 필요
+            Greet greet = greetService.temporarySaveGreet(createGreetRequestDTO);
+            responseMap.put("success", true);
+            responseMap.put("message", "초대장을 성공적으로 임시 저장하였습니다.");
+            responseMap.put("greetId", greet.getId());
+        } catch(Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
         }
-        // TODO: 초대장 저장 후 redirection url 재설정 필요
-        return "redirect:/";
+        return ResponseEntity.ok(responseMap);
+    }
+
+    @PostMapping("/greet/save")
+    @PreAuthorize("hasAnyAuthority()")
+    public ResponseEntity<Object> saveGrit(@RequestBody UpdateGreetRequestDTO updateGreetRequestDTO) {
+        Map<String, Object> responseMap = new HashMap<>();
+        try {
+            Greet greet = greetService.saveGreet(updateGreetRequestDTO);
+            responseMap.put("greetId", greet.getId());
+        } catch (UserPrincipalNotFoundException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+        responseMap.put("success", true);
+        responseMap.put("message", "초대장을 성공적으로 저장하였습니다.");
+        return ResponseEntity.ok(responseMap);
     }
 }
--- src/main/java/com/lgsk/imgreet/greet/service/GreetService.java
+++ src/main/java/com/lgsk/imgreet/greet/service/GreetService.java
@@ -1,15 +1,19 @@
 package com.lgsk.imgreet.greet.service;
 
+import com.lgsk.imgreet.base.commonUtil.Rq;
 import com.lgsk.imgreet.base.entity.Role;
 import com.lgsk.imgreet.entity.Greet;
-import com.lgsk.imgreet.greet.dto.GreetRequestDTO;
-import com.lgsk.imgreet.greet.model.GreetResponseDTO;
+import com.lgsk.imgreet.entity.User;
+import com.lgsk.imgreet.greet.dto.CreateGreetRequestDTO;
+import com.lgsk.imgreet.greet.dto.GreetResponseDTO;
+import com.lgsk.imgreet.greet.dto.UpdateGreetRequestDTO;
 import com.lgsk.imgreet.greet.repository.GreetRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.naming.LimitExceededException;
+import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.List;
 
 @Service
@@ -18,22 +22,44 @@ public class GreetService {
 
     private final GreetRepository greetRepository;
 
-    private final GreetService self;
+    private final Rq rq;
 
     @Transactional
-    public Greet saveGreet(GreetRequestDTO dto) throws LimitExceededException {
-        Long userGreetCount = countGreetsByUserId(dto.getUser().getId());
-        if (isFreeUser(dto.getUser().getRole()) &&  userGreetCount >= 3) {
-            throw new LimitExceededException("무료 사용자의 초대장 생성 제한을 초과하였습니다.");
+    public Greet saveGreet(UpdateGreetRequestDTO dto) throws UserPrincipalNotFoundException {
+        User user = rq.getUser();
+        if (user == null) {
+            throw new UserPrincipalNotFoundException("로그인된 유저 정보가 존재하지 않습니다.");
         }
         return greetRepository.save(
                 Greet.builder()
-                        .user(dto.getUser())
+                        .id(dto.getId())
+                        .user(user)
                         .title(dto.getTitle())
                         .url(dto.getUrl())
                         .imageUrl(dto.getImageUrl())
                         .expireDate(dto.getExpireDate())
-                        .allowComments(dto.isAllowComments())
+                        .allowComments(dto.getAllowComments())
+                        .build());
+    }
+
+    @Transactional
+    public Greet temporarySaveGreet(CreateGreetRequestDTO dto) throws LimitExceededException {
+        User user = rq.getUser();
+        if (user == null) {
+            throw new RuntimeException("사용자 정보가 존재하지 않습니다.");
+        }
+
+        int userGreetCount = countGreetsByUserId(user.getId());
+        if (isFreeUser(user.getRole()) &&  userGreetCount >= 3) {
+            throw new LimitExceededException("무료 사용자의 초대장 생성 제한을 초과하였습니다.");
+        }
+
+        return greetRepository.save(
+                Greet.builder()
+                        .user(user)
+                        .title(dto.getTitle())
+                        .expireDate(dto.getExpireDate())
+                        .allowComments(dto.getAllowComments())
                         .build());
     }
 
@@ -55,13 +81,9 @@ public class GreetService {
                                 .url(e.getUrl())
                                 .imageUrl(e.getImageUrl())
                                 .expireDate(e.getExpireDate())
-                                .allowComments(e.isAllowComments())
+                                .allowComments(e.getAllowComments())
                                 .build())
                 .toList();
-    }
-
-    private int countGreetsByUserId(Long userId) {
-        return self.getGreetByUserId(userId).size();
     }
 
     @Transactional(readOnly = true)
@@ -69,7 +91,11 @@ public class GreetService {
         return greetRepository.findByUser_Id(userId);
     }
 
+    private int countGreetsByUserId(Long userId) {
+        return getGreetByUserId(userId).size();
+    }
+
     private boolean isFreeUser(Role userRole) {
-        return userRole.equals(Role.FREE_USER);
+        return !userRole.equals(Role.PAID_USER);
     }
 }
