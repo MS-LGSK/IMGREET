e90b3a6e6cfe9e61447e646bc10cf3edfcec38da
havegrit <79460763+havegrit@users.noreply.github.com>
2024-08-02 03:12:01 +0900

    Feature/#73/generate and save share url (#79)
    * apply setter
    
    * feat: 공유 url 생성(jwt 라이브러리 이용)
    
    * feat: ajax를 이용하여 초대장 임시 저장 및 저장
    
    * feat: 초대장 상세 엔드포인트 생성
    
    * fix: modify redirect url

--- src/main/java/com/lgsk/imgreet/greet/controller/GreetController.java
+++ src/main/java/com/lgsk/imgreet/greet/controller/GreetController.java
@@ -2,17 +2,28 @@ package com.lgsk.imgreet.greet.controller;
 
 import com.lgsk.imgreet.entity.Greet;
 import com.lgsk.imgreet.greet.dto.CreateGreetRequestDTO;
+import com.lgsk.imgreet.greet.dto.GreetResponseDTO;
 import com.lgsk.imgreet.greet.dto.UpdateGreetRequestDTO;
 import com.lgsk.imgreet.greet.service.GreetService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -21,18 +32,58 @@ public class GreetController {
 
     private final GreetService greetService;
 
+    @GetMapping("/share/{token}")
+    public ResponseEntity<String> sharePage(@PathVariable String token, Model model) {
+        Jwt jwt = Jwt.withTokenValue(token).build();
+        Long greetId = (Long) jwt.getClaims().get("greetId");
+        model.addAttribute("greetId", greetId);
+
+        // TODO: remove hard coding
+        String greetUrl = "http://localhost:8080/greet/share" + greetId;
+
+        // TODO: RestTemplate DI 적용
+        ResponseEntity<String> responseEntity = new RestTemplate().exchange(
+                greetUrl,
+                HttpMethod.GET,
+                new HttpEntity<>(new HttpHeaders()),
+                String.class
+        );
+
+        return ResponseEntity.status(responseEntity.getStatusCode())
+                .body(responseEntity.getBody());
+    }
+
+    @GetMapping("/greet/share/{id}")
+    public String showGreet(@PathVariable Long id, Model model) {
+        GreetResponseDTO responseDTO = greetService.getGreetById(id);
+        model.addAttribute("greet", responseDTO);
+        return "share";
+    }
+
+    @GetMapping("/greet/myGreet")
+    public ResponseEntity<Object> getMyGreet(@Param("id") Long userId) {
+        List<Greet> greetList = greetService.getGreetByUserId(userId);
+        if (greetList.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(greetList);
+    }
+
     @PostMapping("/greet/draft")
     @PreAuthorize("hasAnyAuthority()")
     public ResponseEntity<Object> temporarySaveGrit(@RequestBody CreateGreetRequestDTO createGreetRequestDTO) {
         Map<String, Object> responseMap = new HashMap<>();
         try {
             Greet greet = greetService.temporarySaveGreet(createGreetRequestDTO);
-            responseMap.put("success", true);
-            responseMap.put("message", "초대장을 성공적으로 임시 저장하였습니다.");
             responseMap.put("greetId", greet.getId());
+            responseMap.put("userId", greet.getUser().getId());
         } catch(Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+            responseMap.put("success", false);
+            responseMap.put("message", e.getMessage());
+            return ResponseEntity.badRequest().body(responseMap);
         }
+        responseMap.put("success", true);
+        responseMap.put("message", "초대장을 성공적으로 임시 저장하였습니다.");
         return ResponseEntity.ok(responseMap);
     }
 
@@ -44,7 +95,9 @@ public class GreetController {
             Greet greet = greetService.saveGreet(updateGreetRequestDTO);
             responseMap.put("greetId", greet.getId());
         } catch (UserPrincipalNotFoundException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+            responseMap.put("success", false);
+            responseMap.put("message", e.getMessage());
+            return ResponseEntity.badRequest().body(responseMap);
         }
         responseMap.put("success", true);
         responseMap.put("message", "초대장을 성공적으로 저장하였습니다.");
