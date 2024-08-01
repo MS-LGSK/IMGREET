4df03bb5ae6f74be57fd46ccf22c0dd583a1ea0f
이다영 <yooung513@gmail.com>
2024-08-02 08:50:26 +0900

    Update 테스트 이름 변경

--- src/test/java/com/lgsk/imgreet/admin/AdminDataTests.java
+++ src/test/java/com/lgsk/imgreet/admin/AdminDataTests.java
@@ -2,21 +2,42 @@ package com.lgsk.imgreet.admin;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.lgsk.imgreet.admin.repository.GreetReportRepository;
 import com.lgsk.imgreet.base.entity.Role;
+import com.lgsk.imgreet.entity.Greet;
+import com.lgsk.imgreet.entity.GreetReport;
 import com.lgsk.imgreet.entity.User;
+import com.lgsk.imgreet.greet.repository.GreetRepository;
 import com.lgsk.imgreet.login.repository.UserRepository;
 
 import jakarta.transaction.Transactional;
 
 @SpringBootTest
-public class AdminTests {
+public class AdminDataTests {
 
 	@Autowired private UserRepository userRepository;
+	@Autowired private GreetRepository greetRepository;
+	@Autowired private GreetReportRepository greetReportRepository;
+
+	User badUser;
+
+	@BeforeEach
+	void setup() {
+
+		badUser = userRepository.save(User.builder()
+			.oauthId("kakao_403")
+			.nickname("Bad Creator")
+			.role(Role.FREE_USER)
+			.build());
+	}
 
 	@Test
 	@Transactional
@@ -50,5 +71,27 @@ public class AdminTests {
 
 		User checkUser = userRepository.findByNickname(name);
 		assertThat(checkUser.getRole()).isEqualTo(Role.FREE_USER);
+	}
+
+	@Test
+	@org.springframework.transaction.annotation.Transactional
+	@DisplayName("초대장 신고 더미 생성")
+	void makeGreetReport() {
+		for (int i = 0; i < 100; i++) {
+			Greet greet = greetRepository.save(Greet.builder()
+				.user(badUser)
+				.title("더미" + i)
+				.url("url" + i)
+				.expireDate(LocalDateTime.now().plusDays(5))
+				.allowComments(true)
+				.build());
+
+			greetReportRepository.save(GreetReport.builder()
+				.reason("신고")
+				.ipAddress("ip" + i)
+				.greet(greet)
+				.done(false)
+				.build());
+		}
 	}
 }
