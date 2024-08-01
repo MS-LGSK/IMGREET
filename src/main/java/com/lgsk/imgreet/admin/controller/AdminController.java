dc15ce739fa96dad84c62be3f8e797ce7ada67be
이다영 <yooung513@gmail.com>
2024-08-02 02:51:25 +0900

    Update 접근 권한 Security 사용

--- src/main/java/com/lgsk/imgreet/admin/controller/AdminController.java
+++ src/main/java/com/lgsk/imgreet/admin/controller/AdminController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,8 +18,6 @@ import com.lgsk.imgreet.admin.DTO.CommentReportDTO;
 import com.lgsk.imgreet.admin.DTO.GreetReportDTO;
 import com.lgsk.imgreet.admin.DTO.TemplateReportDTO;
 import com.lgsk.imgreet.admin.service.AdminService;
-import com.lgsk.imgreet.base.commonUtil.Rq;
-import com.lgsk.imgreet.base.entity.Role;
 
 import lombok.RequiredArgsConstructor;
 
@@ -27,18 +26,18 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AdminController {
 
-	private final Rq rq;
 	private final AdminService adminService;
 
 	private static final int PAGE_SIZE = 10;
 
+	@GetMapping()
+	public String adminPage() {
+		return "redirect:/adminPage/GreetReport";
+	}
+
 	// 신고 초대장 관리 - 신고 초대장 목록 가져오기
 	@GetMapping("/GreetReport")
-	public String getGreetReportList (@RequestParam(defaultValue = "0") int page, Model model) throws IllegalAccessException {
-
-		if (rq.getUser().getRole() != Role.ADMIN) {
-			throw new IllegalAccessException("잘못된 접근입니다.");
-		}
+	public String getGreetReportList (@RequestParam(defaultValue = "0") int page, Model model) {
 
 		Pageable pageable = PageRequest.of(page, PAGE_SIZE);
 		Page<GreetReportDTO> greetReportDTOPage = adminService.getGreetReportList(pageable);
@@ -52,10 +51,7 @@ public class AdminController {
 
 	// 신고 템플릿 관리
 	@GetMapping("/TemplateReport")
-	public String getTemplateReport(Model model) throws IllegalAccessException {
-		if (rq.getUser().getRole() != Role.ADMIN) {
-			throw new IllegalAccessException("잘못된 접근입니다.");
-		}
+	public String getTemplateReport(Model model) {
 
 		List<TemplateReportDTO> templateReportDTOList = adminService.getTemplateReportList();
 
@@ -68,11 +64,7 @@ public class AdminController {
 
 	// 신고 댓글 관리 - 신고 댓글 목록 가져오기
 	@GetMapping("/CommentReport")
-	public String getCommentReport(Model model) throws IllegalAccessException {
-
-		if (rq.getUser().getRole() != Role.ADMIN) {
-			throw new IllegalAccessException("잘못된 접근입니다.");
-		}
+	public String getCommentReport(Model model) {
 
 		List<CommentReportDTO> commentReportDTOList = adminService.getCommentReportList();
 
