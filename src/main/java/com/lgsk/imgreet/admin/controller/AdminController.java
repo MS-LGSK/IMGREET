c8237780d0f2ca3ba87656fb1820b84f74c3f54a
이다영 <yooung513@gmail.com>
2024-08-02 13:20:12 +0900

    Update 기능 구현

--- src/main/java/com/lgsk/imgreet/admin/controller/AdminController.java
+++ src/main/java/com/lgsk/imgreet/admin/controller/AdminController.java
@@ -1,16 +1,13 @@
 package com.lgsk.imgreet.admin.controller;
 
-import java.util.List;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -49,13 +46,14 @@ public class AdminController {
 	}
 
 
-	// 신고 템플릿 관리
+	// 신고 템플릿 관리 - 신고 템플릿 목록 가져오기
 	@GetMapping("/TemplateReport")
-	public String getTemplateReport(Model model) {
+	public String getTemplateReport(@RequestParam(defaultValue = "0") int page, Model model) {
 
-		List<TemplateReportDTO> templateReportDTOList = adminService.getTemplateReportList();
+		Pageable pageable = PageRequest.of(page, PAGE_SIZE);
+		Page<TemplateReportDTO> templateReportDTOPage = adminService.getTemplateReportList(pageable);
 
-		model.addAttribute("templateReportList", templateReportDTOList);
+		model.addAttribute("templateReportPage", templateReportDTOPage);
 		model.addAttribute("current", "Template");
 
 		return "adminPage";
@@ -64,22 +62,59 @@ public class AdminController {
 
 	// 신고 댓글 관리 - 신고 댓글 목록 가져오기
 	@GetMapping("/CommentReport")
-	public String getCommentReport(Model model) {
+	public String getCommentReport(@RequestParam(defaultValue = "0") int page, Model model) {
 
-		List<CommentReportDTO> commentReportDTOList = adminService.getCommentReportList();
+		Pageable pageable = PageRequest.of(page, PAGE_SIZE);
+		Page<CommentReportDTO> commentReportDTOPage = adminService.getCommentReportList(pageable);
 
-		model.addAttribute("commentReportList", commentReportDTOList);
+		model.addAttribute("commentReportPage", commentReportDTOPage);
 		model.addAttribute("current", "Comment");
 
 		return "adminPage";
 	}
 
 
-
-
 	// 신고 초대장 삭제
-	@DeleteMapping("/deleteGreet/{greetId}")
-	public String deleteGreet(@PathVariable Long GreetId) {
-		return "adminPage";
+	@PostMapping("/deleteGreet/{greetId}")
+	public String deleteGreet(@PathVariable Long greetId, @RequestParam Long userId) {
+		adminService.deleteGreet(greetId, userId);
+		return "redirect:/adminPage/GreetReport";
+	}
+
+	// 신고 초대장 취소
+	@PostMapping("/updateGreet/{greetId}")
+	public String updateGreet(@PathVariable Long greetId) {
+		adminService.updateGreet(greetId);
+		return "redirect:/adminPage/GreetReport";
+	}
+
+
+	// 신고 템플릿 삭제
+	@PostMapping("/deleteTemplate/{templateId}")
+	public String deleteTemplate(@PathVariable Long templateId, @RequestParam Long userId) {
+		adminService.deleteTemplate(templateId, userId);
+		return "redirect:/adminPage/TemplateReport";
+	}
+
+	// 신고 템플릿 취소
+	@PostMapping("/updateTemplate/{templateId}")
+	public String updateTemplate(@PathVariable Long templateId) {
+		adminService.updateTemplate(templateId);
+		return "redirect:/adminPage/TemplateReport";
+	}
+
+
+	// 신고 댓글 삭제
+	@PostMapping("/deleteComment/{commentId}")
+	public String deleteComment(@PathVariable Long commentId) {
+		adminService.deleteComment(commentId);
+		return "redirect:/adminPage/CommentReport";
+	}
+
+	// 신고 댓글 취소
+	@PostMapping("/updateComment/{commentId}")
+	public String updateComment(@PathVariable Long commentId) {
+		adminService.updateComment(commentId);
+		return "redirect:/adminPage/CommentReport";
 	}
 }
--- src/main/java/com/lgsk/imgreet/admin/service/AdminService.java
+++ src/main/java/com/lgsk/imgreet/admin/service/AdminService.java
@@ -1,8 +1,5 @@
 package com.lgsk.imgreet.admin.service;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -14,16 +11,19 @@ import com.lgsk.imgreet.admin.DTO.GreetReportDTO;
 import com.lgsk.imgreet.admin.DTO.GreetReportResponseDTO;
 import com.lgsk.imgreet.admin.DTO.TemplateReportDTO;
 import com.lgsk.imgreet.admin.DTO.TemplateReportResponseDTO;
-import com.lgsk.imgreet.admin.repository.CommnetReportRepository;
+import com.lgsk.imgreet.admin.repository.CommentReportRepository;
 import com.lgsk.imgreet.admin.repository.GreetReportRepository;
 import com.lgsk.imgreet.admin.repository.TemplateReportRepository;
+import com.lgsk.imgreet.base.entity.Role;
 import com.lgsk.imgreet.comment.repository.CommentRepository;
 import com.lgsk.imgreet.entity.Comment;
 import com.lgsk.imgreet.entity.Greet;
 import com.lgsk.imgreet.entity.Template;
 import com.lgsk.imgreet.greet.repository.GreetRepository;
+import com.lgsk.imgreet.login.repository.UserRepository;
 import com.lgsk.imgreet.template.repository.TemplateRepository;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -31,11 +31,12 @@ import lombok.RequiredArgsConstructor;
 public class AdminService {
 
 	private final GreetReportRepository greetReportRepository;
-	private final CommnetReportRepository commentReportRepository;
+	private final CommentReportRepository commentReportRepository;
 	private final TemplateReportRepository templateReportRepository;
 	private final GreetRepository greetRepository;
 	private final CommentRepository commentRepository;
 	private final TemplateRepository templateRepository;
+	private final UserRepository userRepository;
 
 	// 신고 초대장 목록 가져오기
 	@Transactional
@@ -50,7 +51,7 @@ public class AdminService {
 		return GreetReportDTO.builder()
 			.greetId(greetReport.getGreetId())
 			.greetTitle(greet.getTitle())
-			.greetUrl(greet.getTitle())
+			.greetUrl(greet.getUrl())
 			.reason(greetReport.getReason())
 			.userId(greet.getUser().getId())
 			.build();
@@ -59,59 +60,92 @@ public class AdminService {
 
 	// 신고 템플릿 목록 가져오기
 	@Transactional
-	public List<TemplateReportDTO> getTemplateReportList() {
-		List<TemplateReportDTO> templateReportDTOList = new ArrayList<>();
+	public Page<TemplateReportDTO> getTemplateReportList(Pageable pageable) {
+		return templateReportRepository.findDistinctByDone(pageable).map(this::convertToTemplateDTO);
+	}
 
-		List<TemplateReportResponseDTO> templateReportList = templateReportRepository.findDistinctByDone();
-		for (TemplateReportResponseDTO templateReport : templateReportList) {
-			Template template = templateRepository.findById(templateReport.getTemplateId())
-				.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 템플릿입니다."));
+	private TemplateReportDTO convertToTemplateDTO (TemplateReportResponseDTO templateReport) {
+		Template template = templateRepository.findById(templateReport.getTemplateId())
+			.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 템플릿입니다."));
 
-			TemplateReportDTO templateReportDTO = TemplateReportDTO.builder()
-				.templateId(template.getId())
-				.templateTitle(template.getTitle())
-				.templateURL("templateUrl")
-				.reason(templateReport.getReason())
-				.creatorId(template.getCreatorId())
-				.build();
-
-			templateReportDTOList.add(templateReportDTO);
-		}
-
-		return templateReportDTOList;
+		return TemplateReportDTO.builder()
+			.templateId(template.getId())
+			.templateTitle(template.getTitle())
+			.templateURL("/template/" + template.getId())
+			.creatorId(template.getCreatorId())
+			.reason(templateReport.getReason())
+			.build();
 	}
 
 
 	// 신고 댓글 목록 가져오기
 	@Transactional
-	public List<CommentReportDTO> getCommentReportList() {
-		List<CommentReportDTO> commentReportDTOList = new ArrayList<>();
+	public Page<CommentReportDTO> getCommentReportList(Pageable pageable) {
+		return commentReportRepository.findDistinctByDone(pageable).map(this::convertToCommentDTO);
+	}
+	private CommentReportDTO convertToCommentDTO (CommentReportResponseDTO commentReport) {
+		Comment comment = commentRepository.findById(commentReport.getCommentId())
+			.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 댓글입니다."));
 
-		List<CommentReportResponseDTO> commentReportList = commentReportRepository.findDistinctByDone();
-		for (CommentReportResponseDTO commentReport : commentReportList) {
-			Comment comment = commentRepository.findById(commentReport.getCommentId())
-				.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 댓글입니다."));
-			Greet greet = comment.getGreet();
-
-			CommentReportDTO commentReportDTO = CommentReportDTO.builder()
-				.greetId(greet.getId())
-				.greetTitle(greet.getTitle())
-				.greetUrl(greet.getUrl())
-				.commentId(comment.getId())
-				.commentContent(comment.getContent())
-				.reportReason(commentReport.getReason())
-				.build();
-
-			commentReportDTOList.add(commentReportDTO);
-		}
-
-		return commentReportDTOList;
+		return CommentReportDTO.builder()
+			.greetId(comment.getGreet().getId())
+			.greetTitle(comment.getGreet().getTitle())
+			.greetUrl(comment.getGreet().getUrl())
+			.commentId(comment.getId())
+			.commentContent(comment.getContent())
+			.reportReason(commentReport.getReason())
+			.build();
 	}
 
 
 	// 신고 초대장 삭제
 	@Transactional
-	public void deleteGreet(Long greetId) {
+	public void deleteGreet(Long greetId, Long userId) {
+		greetReportRepository.greetReportDoneByGreetId(greetId);
+		// greetRepository.deleteById(greetId);
 
+		int updateUserBan = userRepository.updateBanByUserId(userId, Role.BANNED_USER);
+		if(updateUserBan == 0) {
+			throw new EntityNotFoundException("유저 접근 제한에 실패했습니다.");
+		}
+	}
+
+	// 신고 초대장 취소
+	@Transactional
+	public void updateGreet(Long greetId) {
+		greetReportRepository.greetReportDoneByGreetId(greetId);
+	}
+
+
+	// 신고 템플릿 삭제
+	@Transactional
+	public void deleteTemplate(Long templateId, Long userId) {
+		templateReportRepository.templateReportDoneByTemplateId(templateId);
+		// templateReportRepository.deleteById(templateId);
+
+		int updateUserBan = userRepository.updateBanByUserId(userId, Role.BANNED_USER);
+		if(updateUserBan == 0) {
+			throw new EntityNotFoundException("유저 접근 제한에 실패했습니다.");
+		}
+	}
+
+	// 신고 템플릿 취소
+	@Transactional
+	public void updateTemplate (Long templateId) {
+		templateReportRepository.templateReportDoneByTemplateId(templateId);
+	}
+
+
+	// 신고 댓글 삭제
+	@Transactional
+	public void deleteComment(Long commentId) {
+		commentReportRepository.commentReportDoneByCommentId(commentId);
+		// commentRepository.deleteById(commentId);
+	}
+
+	// 신고 댓글 취소
+	@Transactional
+	public void updateComment(Long commentId) {
+		commentReportRepository.commentReportDoneByCommentId(commentId);
 	}
 }
